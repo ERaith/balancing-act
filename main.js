2b587d678620dad0ed99e94b737454be18fef545
Erik Raith <raithhtiar@gmail.com>
2019-11-25 12:47:45 -0700

    Add hidden element persist on Welcome Banner

--- main.js
+++ main.js
@@ -1,44 +1,74 @@
 //Function to hide the wecomeBanner.
-function myFunction() {
+//localStorage sessionStorage
+
+// if(isHidden==true){
+//  document.getElementsByClassName('welcomeBanner').style.display ='none';
+// }
+// sessionStorage.setItem('isHidden',false)
+let data = sessionStorage.getItem('hidden');
+if(data === 'true'){
+  document.getElementById('welcomeBanner').style.display ="none";
+}else{
+  document.getElementById('welcomeBanner').style.display ="block";
+}
+// sessionStorage.setItem('hidden','true')
+function hideBanner() {
   var x = document.getElementById('welcomeBanner');
   x.style.display = "none";
+  sessionStorage.setItem('hidden', 'true');
+
 }
 //Function to add/remove active class based on url.
-window.onload = function() {
-    var assetActive = document.querySelector('#assets');
-    var transActive = document.querySelector('#transactions');
-    var proActive = document.querySelector('#profile');
-    assetActive.addEventListener('click', function(){
-      setActive(assetActive);
-    });
-    transActive.addEventListener('click', function(){
-      setActive(transActive);
-    });
-    proActive.addEventListener('click', function(){
-      setActive(proActive);
-    });
-    //transActive.addEventListener('click', setActive(2));
-    // proActive.addEventListener('click', setActive(3));
+  var assetActive = document.getElementById('assets');
+  var transActive = document.getElementById('transactions');
+  var proActive = document.getElementById('profile');
 
-    function setActive(activeLink) {
-          proActive.classList.remove('activeLink');
-          assetActive.classList.remove('activeLink');
-          transActive.classList.remove('activeLink');
-          activeLink.classList.add('activeLink');
-        }
-      }
+  assetActive.addEventListener('click', function() {
+    setActive(assetActive);
+  });
+  transActive.addEventListener('click', function() {
+    setActive(transActive);
+  });
+  proActive.addEventListener('click', function() {
+    setActive(proActive);
+  });
 
+  function setActive(activeLink) {
+    proActive.classList.remove('activeLink');
+    assetActive.classList.remove('activeLink');
+    transActive.classList.remove('activeLink');
+    activeLink.classList.add('activeLink');
+  }
 
-    /*
+//Table JavaScript to put the table wherever I want.
+function json2table(json, classes) {
+  var cols = Object.keys(json[0]);
+  var headerRow = '';
+  var bodyRows = '';
+  //checks to make sure classes is initiallized with either class or an empty string
+  classes = classes || '';
 
-    var navLinks = document.querySelectorAll('li a');
-     url = window.location.href;
-
-    if (url.includes("#profile")) {
-      navLinks[0].classList.add('activeLink');
-    } else if(url.includes("#transactions")) {
-      navLinks[1].classList.add('activeLink');
-    } else {
-      navLinks[2].classList.add('activeLink');
-    }
-    */
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  //Map HeaderRow of table from Json.
+  cols.map(function(col) {
+    headerRow += `<th>${capitalizeFirstLetter(col)}</th>`;
+    // headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
+  })
+  json.map(function(row) {
+    bodyRows += '<tr>';
+    cols.map(function(colName) {
+      bodyRows += '<td>' + row[colName] + '</td>';
+    })
+    bodyRows += '</tr>';
+  });
+  return '<table class="' +
+    classes +
+    '"><thead><tr>' +
+    headerRow +
+    '</tr></thead><tbody>' +
+    bodyRows +
+    '</tbody></table>';
+}
+// json2table(data/finances.json)
